012ed1f576ef000d5a56b549b7e349fa9f6abe46
Ramanshk <ramanshakya101@gmail.com>
2022-01-09 20:37:48 +0545

    Category dropdown added to admin view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,41 @@
 
 @section('body')
     <div class="max-w-7xl mx-auto">
-        <table class="bg-gray-100 w-full border mt-20 mb-4">
+        {{-- <h1 class=" font-semibold text-5xl mt-10">
+            <a href="/all-products">All products</a> 
+            @if (request('category') ?? false)
+                @if (request('category') == "Book")
+                    / Books
+                @elseif (request('category') == "Cd")
+                    / Cds
+                @else
+                    / Games
+                @endif
+            @endif
+        </h1> --}}
+        <div class="flex justify-between w-full mt-8 text-black text-5xl font-semibold">
+            <h1>
+                <a href="/products">All products</a> 
+                @if (request('category') ?? false)
+                    @if (request('category') == "Book")
+                        / Books
+                    @elseif (request('category') == "Cd")
+                        / Cds
+                    @else
+                        / Games
+                    @endif
+                @endif
+            </h1>
+            <form action="" method="get">
+                <select name="category" onchange="this.form.submit()">
+                    <option selected disabled="disabled">Select a category</option>
+                    <option value="Book" @if (request('category') == "Book") selected @endif>Books</option>
+                    <option value="Cd" @if (request('category') == "Cd") selected @endif>Cds</option>
+                    <option value="Game" @if (request('category') == "Game") selected @endif>Games</option>
+                </select>
+            </form>
+        </div>
+        <table class="bg-gray-100 w-full border mb-4 mt-8">
             <tr class="text-center text-lg h-10">
                 <td class="font-bold w-60 border">Title</td>
                 <td class="font-bold w-60 border">Name</td>
